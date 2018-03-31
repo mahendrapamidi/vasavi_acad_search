d908065ae5f035280dfa29acdb46d8f491d71dad
Mahendra pamidi <mahendrapamidi96@gmail.com>
2018-03-31 21:46:50 +0530

    uploads

--- acads/urls.py
+++ acads/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^$', views.home, name='home'),
+    url(r'^uploads/simple/$', views.simple_upload, name='simple_upload'),
+    url(r'^uploads/form/$', views.model_form_upload, name='model_form_upload'),
+    url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- acads/views.py
+++ acads/views.py
@@ -1,11 +1,36 @@
-from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'acads/post_list.html', {'posts': posts})
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'acads/post_detail.html', {'post': post})    
+from acads.models import Document
+from acads.forms import DocumentForm
+
+
+def home(request):
+    documents = Document.objects.all()
+    return render(request, 'acads/home.html', { 'documents': documents })
+
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'acads/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'acads/simple_upload.html')
+
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'acads/model_form_upload.html', {
+        'form': form
+    })
